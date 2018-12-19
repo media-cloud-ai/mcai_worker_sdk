ccfa49318959c434bbb863b932744e6535a6b6c9
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-12-19 13:57:49 +0100

    update code based on clippy messages

--- src/job.rs
+++ src/job.rs
@@ -38,17 +38,14 @@ pub struct Job {
 impl Job {
   pub fn get_string_parameter(&self, key: &str) -> Option<String> {
     for param in self.parameters.iter() {
-      match param {
-        Parameter::StringParam { id, default, value } => {
-          if id == key {
-            if let Some(ref v) = value {
-              return Some(v.clone());
-            } else {
-              return default.clone();
-            }
+      if let Parameter::StringParam { id, default, value } = param {
+        if id == key {
+          if let Some(ref v) = value {
+            return Some(v.clone());
+          } else {
+            return default.clone();
           }
         }
-        _ => {}
       }
     }
     None
@@ -56,23 +53,20 @@ impl Job {
 
   pub fn check_requirements(&self) -> Result<(), MessageError> {
     for param in self.parameters.iter() {
-      match param {
-        Parameter::RequirementParam { id, value, .. } => {
-          if id == "requirements" {
-            if let Some(Requirement { paths: Some(paths) }) = value {
-              for ref path in paths.iter() {
-                let p = Path::new(path);
-                if !p.exists() {
-                  return Err(MessageError::RequirementsError(format!(
-                    "Warning: Required file does not exists: {:?}",
-                    p
-                  )));
-                }
+      Parameter::RequirementParam { id, value, .. } = param {
+        if id == "requirements" {
+          if let Some(Requirement { paths: Some(paths) }) = value {
+            for path in &paths.iter() {
+              let p = Path::new(path);
+              if !p.exists() {
+                return Err(MessageError::RequirementsError(format!(
+                  "Warning: Required file does not exists: {:?}",
+                  p
+                )));
               }
             }
           }
         }
-        _ => {}
       }
     }
     Ok(())
