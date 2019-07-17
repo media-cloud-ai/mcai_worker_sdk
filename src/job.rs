40db1f47e04f23fbc0adac04e275c46435e81f47
Marc-Antoine Arnaud <maarnaud@media-io.com>
2019-07-17 16:27:16 +0200

    remove static from API

--- src/job.rs
+++ src/job.rs
@@ -130,7 +130,7 @@ pub enum Parameter {
   },
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Job {
   pub job_id: u64,
   pub parameters: Vec<Parameter>,
@@ -171,14 +171,16 @@ struct ValueResponseBody {
 }
 
 impl Credential {
-  pub fn request_value(&self, job: &'static Job) -> Result<String, MessageError> {
+  pub fn request_value(&self, job: &Job) -> Result<String, MessageError> {
     let backend_endpoint = config::get_backend_hostname();
     let backend_username = config::get_backend_username();
     let backend_password = config::get_backend_password();
 
     let session_url = format!("{}/sessions", backend_endpoint);
     let credential_url = format!("{}/credentials/{}", backend_endpoint, self.key);
-    let job_clone = job.clone();
+
+    let cloned_job = job.clone();
+    let thread_job = job.clone();
 
     let request_thread = thread::spawn(move || {
       let client = reqwest::Client::builder().build().unwrap();
@@ -190,49 +192,32 @@ impl Credential {
         },
       };
 
-      let mut response = client
-        .post(&session_url)
-        .json(&session_body)
-        .send()
-        .map_err(|e| {
-          let job_result = JobResult::from(job_clone)
-            .with_status(JobStatus::Error)
-            .with_error(e);
-          MessageError::ProcessingError(job_result)
-        })?;
+      let request = client.post(&session_url).json(&session_body).send();
+
+      let mut response = check_error(request, &thread_job)?;
 
       let r: SessionResponseBody = response.json().map_err(|e| {
-        let job_result = JobResult::from(job_clone)
+        let job_result = JobResult::from(thread_job.clone())
           .with_status(JobStatus::Error)
           .with_error(e);
         MessageError::ProcessingError(job_result)
       })?;
       let token = r.access_token;
 
-      let mut response = client
+      let request = client
         .get(&credential_url)
         // .bearer_auth(token)
         .header("Authorization", token)
-        .send()
-        .map_err(|e| {
-          let job_result = JobResult::from(job_clone)
-            .with_status(JobStatus::Error)
-            .with_error(e);
-          MessageError::ProcessingError(job_result)
-        })?;
+        .send();
 
-      let resp_value: ValueResponseBody = response.json().map_err(|e| {
-        let job_result = JobResult::from(job_clone)
-          .with_status(JobStatus::Error)
-          .with_error(e);
-        MessageError::ProcessingError(job_result)
-      })?;
+      let response = check_error(request, &thread_job)?;
+      let resp_value = parse_json(response, &thread_job)?;
 
       Ok(resp_value.data.value)
     });
 
     request_thread.join().map_err(|e| {
-      let job_result = JobResult::from(job_clone)
+      let job_result = JobResult::from(cloned_job)
         .with_status(JobStatus::Error)
         .with_message(format!("{:?}", e));
       MessageError::ProcessingError(job_result)
@@ -341,4 +326,22 @@ impl JobResult {
     });
     self
   }
+}
+
+fn check_error<T>(item: Result<T, Error>, job: &Job) -> Result<T, MessageError> {
+  item.map_err(|e| {
+    let job_result = JobResult::from(job.clone())
+      .with_status(JobStatus::Error)
+      .with_error(e);
+    MessageError::ProcessingError(job_result)
+  })
+}
+
+fn parse_json(mut body: reqwest::Response, job: &Job) -> Result<ValueResponseBody, MessageError> {
+  body.json().map_err(|e| {
+    let job_result = JobResult::from(job.clone())
+      .with_status(JobStatus::Error)
+      .with_error(e);
+    MessageError::ProcessingError(job_result)
+  })
 }
