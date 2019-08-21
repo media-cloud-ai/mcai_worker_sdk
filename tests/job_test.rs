6fe33526ca5b62f4d965f9d6d4fd53c7cd10b569
Valentin NOEL <valentin.noel@media-io.com>
2019-08-21 11:32:07 +0200

    Add mockito to test Credential::request_value() function

--- tests/job_test.rs
+++ tests/job_test.rs
@@ -464,3 +464,48 @@ fn test_job_result_with_setters() {
   // let e = std::io::Error::new(std::io::ErrorKind::Other, "oh no!");
   // let job_result = job_result.with_error(reqwest::Error::from(e));
 }
+
+#[test]
+fn test_credential_request_value() {
+  std::env::set_var("BACKEND_HOSTNAME", mockito::server_url());
+  use mockito::mock;
+
+  let _m = mock("POST", "/sessions")
+    .with_header("content-type", "application/json")
+    .with_body(r#"{"access_token": "fake_access_token"}"#)
+    .create();
+
+  let _m = mock("GET", "/credentials/TEST_CREDENTIAL_KEY")
+    .with_header("content-type", "application/json")
+    .with_body(
+      r#"{"data": {
+        "id": 666,
+        "key": "TEST_CREDENTIAL_KEY",
+        "value": "TEST_CREDENTIAL_VALUE",
+        "inserted_at": "today"
+      }}"#,
+    )
+    .create();
+
+  let message = r#"{
+    "job_id": 123,
+    "parameters": [
+      { "id":"test_credential",
+        "type":"credential",
+        "value":"TEST_CREDENTIAL_KEY"
+      }
+    ]
+  }"#;
+
+  let message_parsing = Job::new(message);
+  assert!(message_parsing.is_ok());
+  let job = message_parsing.unwrap();
+  let optional_credential = job.get_credential_parameter("test_credential");
+  assert!(optional_credential.is_some());
+  let credential = optional_credential.unwrap();
+  assert_eq!("TEST_CREDENTIAL_KEY".to_string(), credential.key);
+  let request_result = credential.request_value(&job);
+  assert!(request_result.is_ok());
+  let credential_value = request_result.unwrap();
+  assert_eq!("TEST_CREDENTIAL_VALUE".to_string(), credential_value);
+}
