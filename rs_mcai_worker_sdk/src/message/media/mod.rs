bf6396d4994f3656ba4cfd790293649e7aebcbb0
Valentin NOEL <valentin.noel@media-io.com>
2020-07-21 11:24:33 +0200

    Fix process progress

--- rs_mcai_worker_sdk/src/message/media/mod.rs
+++ rs_mcai_worker_sdk/src/message/media/mod.rs
@@ -47,7 +47,6 @@ pub fn process<P: DeserializeOwned + JsonSchema, ME: MessageEvent<P>>(
         stream_index,
         frame,
       } => {
-        count += 1;
 
         if stream_index == 0 {
           count += 1;
