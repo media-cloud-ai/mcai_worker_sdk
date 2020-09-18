88782c1a3a42bc6a6510f78fcc2f2f21886b9efc
Valentin NOEL <valentin.noel@media-io.com>
2020-09-18 16:15:48 +0200

    Improve Python SDK usage documentation and examples

--- py_mcai_worker_sdk/README.md
+++ py_mcai_worker_sdk/README.md
@@ -18,15 +18,23 @@ cargo test
 
 ## Usage
 
-This worker uses Rust FFI to load a C/C++ Shared Object library, and to execute it. The C/C++ worker must implement some functions to be correctly bound:
+This worker uses the [PyO3 crate](https://github.com/PyO3/pyo3) to load a Python file, and to execute it.
+The Python worker must implement some functions to be correctly bound:
 
- * `String get_name()`: to retrieve the worker name
- * `String get_short_description()`: to retrieve a short description of the worker
- * `String get_description()`: to describe the worker purpose
- * `String get_version()`: to retrieve the worker version
- * `Array<Parameter> get_parameters() `: return the list of parameters for this worker
- * `void process(parameters)`: to execute the worker process
+ * `get_name() -> str`: to retrieve the worker name
+ * `get_short_description() -> str`: to retrieve a short description of the worker
+ * `get_description() -> str`: to describe the worker purpose
+ * `get_version() -> str`: to retrieve the worker version
+ * `init()`: to initialize the worker process (optional)
+ * `process(handle_callback, parameters) -> dict`: to execute the worker process and return the job result
 
-For more details, see the provided [worker.py](worker.py) example.
+If the `media` feature is enabled, the following function must be implemented:
+ * `init_process(stream_handler, format_context, parameters)`: to initialize the media worker process
+ * `process_frame(job_id, stream_index, frame)`: to process an input audio/video frame
+ * `ending_process(parameters)`: to end the media worker process
+
+__NB:__ the `process(handle_callback, parameters)` function is not called when the `media` feature is enabled. 
+
+For more details, see the provided [worker.py](worker.py) and [media_worker.py](media_worker.py) examples.
 
 Set the `PYTHON_WORKER_FILENAME` environment variable to specify the path of your Python worker. Otherwise, the `worker.py` file will be loaded by default.
---
+++ py_mcai_worker_sdk/media_worker.py
@@ -0,0 +1,116 @@
+import json
+import logging
+import os
+
+def get_name():
+    return "My python Media Worker"
+
+
+def get_short_description():
+    return "My python Media Worker"
+
+
+def get_description():
+    return """This is my long description
+	over multilines
+	"""
+
+
+def get_version():
+    return "0.0.3"
+
+
+def get_parameters():
+    return [
+        {
+            "identifier": "source_path",
+            "label": "My parameter",
+            "kind": ["string"],
+            "required": True,
+        },
+        {
+            "identifier": "destination_path",
+            "label": "My array parameter",
+            "kind": ["string"],
+            "required": False,
+        }
+    ]
+
+
+def init():
+    '''
+    Optional worker initialization function.
+    '''
+
+    print("Initialise Python worker...")
+
+    log_level = os.environ.get('RUST_LOG', 'warning').upper()
+    logging.basicConfig(format='[%(levelname)s] %(message)s', level=log_level)
+
+
+def init_process(stream_handler, format_context, parameters):
+    '''
+    Function called before the media process (the "media" feature must be activated).
+    '''
+    logging.info("Initialise the media process...")
+    logging.debug("Number of streams: %d", format_context.nb_streams)
+    logging.debug("Message parameters: %s", parameters)
+
+    # Here audio/video filters can be set to be applied on the worker input frames, using a simple python dict as follow.
+    # Check the FFmpeg documentation to have more details on filters usage: https://ffmpeg.org/ffmpeg-filters.html
+    video_filters = [
+        {
+            "name": "crop",
+            "label": "crop_filter",
+            "parameters": {
+               "out_w": "300",
+               "out_h": "200",
+               "x": "50",
+               "y": "50"
+            }
+        }
+    ]
+
+    audio_filters = [
+        {
+            "name": "aformat",
+            "parameters": {
+                "sample_rates": "16000",
+                "channel_layouts": "mono",
+                "sample_fmts": "s16"
+            }
+        }
+    ]
+
+    video_stream = stream_handler.new_video_stream(0, video_filters)
+    audio_stream = stream_handler.new_audio_stream(1, audio_filters)
+
+    # returns a list of description of the streams to be processed
+    return [
+        video_stream,
+        audio_stream
+    ]
+
+
+def process_frame(job_id, stream_index, frame):
+    '''
+    Process media frame (the "media" feature must be activated).
+    '''
+    data_length = 0
+    for plane in range(0, len(frame.data)):
+        data_length = data_length + len(frame.data[plane])
+
+    if frame.width != 0 and frame.height != 0:
+        logging.info(f"Job: {job_id} - Process video stream {stream_index} frame - PTS: {frame.pts}, image size: {frame.width}x{frame.height}, data length: {data_length}")
+    else:
+        logging.info(f"Job: {job_id} - Process audio stream {stream_index} frame - PTS: {frame.pts}, sample_rate: {frame.sample_rate}Hz, channels: {frame.channels}, nb_samples: {frame.nb_samples}, data length: {data_length}")
+
+    # returns the process result as a JSON object (this is fully customisable)
+    return { "status": "success" }
+
+
+def ending_process():
+    '''
+    Function called at the end of the media process (the "media" feature must be activated).
+    '''
+    logging.info("Ending Python worker process...")
