3cab9d526ee9d0fb7c0243d36e0e80d465a677ec
Valentin NOEL <valentin.noel@media-io.com>
2021-04-07 16:59:05 +0200

    Handle StopConsumingJobs and ResumeConsumingJobs orders in the jobs consumer

--- rs_mcai_worker_sdk/src/message_exchange/rabbitmq/consumer.rs
+++ rs_mcai_worker_sdk/src/message_exchange/rabbitmq/consumer.rs
@@ -2,10 +2,10 @@ use super::{helpers, publish, CurrentOrders};
 use crate::{message_exchange::OrderMessage, MessageError, Result};
 use amq_protocol_types::FieldTable;
 use async_std::{
-  stream::StreamExt,
   channel::{Receiver, Sender},
   task::{self, JoinHandle},
 };
+use futures_util::stream::TryStreamExt;
 use lapin::{
   message::Delivery,
   options::{BasicAckOptions, BasicCancelOptions, BasicConsumeOptions, BasicRejectOptions},
@@ -14,6 +14,7 @@ use lapin::{
 use std::{
   convert::TryFrom,
   sync::{Arc, Mutex},
+  time::Duration,
 };
 
 pub struct RabbitmqConsumer {
@@ -35,7 +36,7 @@ impl RabbitmqConsumer {
   ) -> Result<Self> {
     log::debug!("Start RabbitMQ consumer on queue {:?}", queue_name);
 
-    let mut consumer = channel
+    let consumer = channel
       .basic_consume(
         queue_name,
         consumer_tag,
@@ -44,29 +45,102 @@ impl RabbitmqConsumer {
       )
       .await?;
 
+    let mut current_consumer = Some(consumer);
+
     let channel = Arc::new(channel.clone());
     let queue_name_clone = queue_name.to_string();
+    let consumer_tag_clone = consumer_tag.to_string();
 
     let handle = Some(task::spawn(async move {
-      while let Some(delivery) = consumer.next().await {
-        let (_, delivery) = delivery.expect("error in consumer");
 
-        if let Err(error) = Self::process_delivery(
-          sender.clone(),
-          channel.clone(),
-          &delivery,
-          &queue_name_clone.clone(),
-          current_orders.clone(),
-          consumer_notification_sender.clone(),
-          consumer_notification_receiver.clone(),
-        )
-        .await
-        {
-          log::error!("RabbitMQ consumer: {:?}", error);
-          if let Err(error) = publish::error(channel.clone(), &delivery, &error).await {
-            log::error!("Unable to publish response: {:?}", error);
+      loop {
+
+        // If there is a receiver for messages forwarded by the other consumer...
+        if let Some(receiver) = consumer_notification_receiver.clone() {
+
+          // Check whether the other consumer forwarded an order message
+          if let Ok(order_from_other_consumer) = receiver.try_recv() {
+            log::debug!(
+              "Queue {} consumer {} got a message from the other consumer...",
+              queue_name_clone,
+              consumer_tag_clone
+            );
+
+            match order_from_other_consumer {
+              OrderMessage::StopConsumingJobs => {
+                // Stop consuming jobs queue
+                log::info!("Stop consuming {} jobs queue", queue_name_clone);
+                if let Err(error) = channel
+                  .basic_cancel(&consumer_tag_clone, BasicCancelOptions::default())
+                  .await
+                {
+                  log::error!(
+                    "Unable to stop consuming {} jobs queue: {}",
+                    queue_name_clone,
+                    error.to_string()
+                  )
+                } else {
+                  current_consumer = None;
+                }
+              }
+              OrderMessage::ResumeConsumingJobs => {
+                // Resume consuming jobs queue
+                log::info!("Resume consuming {} jobs queue", queue_name_clone);
+                match channel
+                  .basic_consume(
+                    &queue_name_clone,
+                    &consumer_tag_clone,
+                    BasicConsumeOptions::default(),
+                    FieldTable::default(),
+                  )
+                  .await
+                {
+                  Ok(new_consumer) => {
+                    current_consumer = Some(new_consumer);
+                  }
+                  Err(error) => log::error!(
+                    "Unable to resume consuming {} jobs queue: {}",
+                    queue_name_clone,
+                    error.to_string()
+                  ),
+                }
+              }
+              _ => log::error!(
+                "Unsupported order message from other consumer: {:?}",
+                order_from_other_consumer
+              ),
+            }
           }
         }
+
+        // If there is an AMQP consumer...
+        if let Some(mut consumer) = current_consumer.clone() {
+
+          // Check whether a message can be consumed from the AMQP channel
+          if let Ok(delivery) = consumer.try_next().await {
+            let (_, delivery) = delivery.expect("error in consumer");
+
+            if let Err(error) = Self::process_delivery(
+              sender.clone(),
+              channel.clone(),
+              &delivery,
+              &queue_name_clone.clone(),
+              current_orders.clone(),
+              consumer_notification_sender.clone(),
+              consumer_notification_receiver.clone(),
+            )
+            .await
+            {
+              log::error!("RabbitMQ consumer: {:?}", error);
+              if let Err(error) = publish::error(channel.clone(), &delivery, &error).await {
+                log::error!("Unable to publish response: {:?}", error);
+              }
+            }
+          }
+        }
+
+        // Wait a bit...
+        std::thread::sleep(Duration::from_millis(40));
       }
     }));
 
@@ -154,8 +228,9 @@ impl RabbitmqConsumer {
         current_orders.lock().unwrap().status = Some(delivery.clone());
       }
       OrderMessage::StopConsumingJobs | OrderMessage::ResumeConsumingJobs => {
-        // Stop consuming jobs queue
+
         if let Some(sender) = consumer_notification_sender {
+          // Forward the received order to the job messages consumer
           sender.send(order_message.clone()).await.map_err(|e| {
             MessageError::RuntimeError(format!(
               "Unable to send {:?} order to other consumer: {:?}",
@@ -167,7 +242,9 @@ impl RabbitmqConsumer {
             .basic_ack(delivery.delivery_tag, BasicAckOptions::default())
             .await
             .map_err(MessageError::Amqp);
-        } else if let Some(_receiver) = consumer_notification_receiver {
+        }
+
+        if let Some(_receiver) = consumer_notification_receiver {
           log::warn!(
             "{:?} queue consumer cannot handle correctly such an order message: {:?}",
             queue_name,
